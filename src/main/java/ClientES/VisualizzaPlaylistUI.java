2ae92f9c3e169afc5d989420d13feaaf6ff669b7
emili <emiliotoli83@gmail.com>
2023-09-02 19:41:47 +0200

    JavaDoc VisualizzaPlaylist

--- src/main/java/ClientES/VisualizzaPlaylistUI.java
+++ src/main/java/ClientES/VisualizzaPlaylistUI.java
@@ -8,9 +8,23 @@ import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class VisualizzaPlaylistUI extends JFrame {
-    private JTextArea playlistTextArea;
+/**
+ * @author Emilio Toli
+ *      Classe che gestisce la visualizzazione
+ *      delle playlist associate ad un utente
+ */
 
+public class VisualizzaPlaylistUI extends JFrame {
+    // <editor-fold desc= "Attributi">
+    private JTextArea playlistTextArea;
+    // </editor-fold>
+
+    /**
+     * @author Emilio Toli
+     *      Metodo per visualizzare le playlist dato il nome
+     *      della playlist inserito in un'area di testo
+     */
+    // <editor-fold desc= "Visualizzazione a video delle playlist">
     public void visualizzaPlaylist() {
         setTitle("Visualizza Playlist");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -33,16 +47,23 @@ public class VisualizzaPlaylistUI extends JFrame {
                 ArrayList<PlayList> playlist = null; // Sostituisci con il metodo corretto
                 try {
                     playlist = Client.visualizzaPlaylist(Client.idGlobale);
-                } catch (RemoteException remoteException) {
+                } catch (RemoteException | SQLException remoteException) {
                     remoteException.printStackTrace();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
                 }
                 updatePlaylistTextArea(playlist);
             }
         });
     }
+    // </editor-fold>
 
+    /**
+     * @author Emilio Toli
+     * @param playlist Lista che contiene tutte le playlist dell'utente
+     *
+     *      Metodo per visualizzare le playlist dell'utente una volta premuto il pulsante
+     */
+
+    // <editor-fold desc= "Visualizza playlist">
     private void updatePlaylistTextArea(ArrayList<PlayList> playlist) {
         playlistTextArea.setText("");
         if (playlist.isEmpty()) {
@@ -53,5 +74,6 @@ public class VisualizzaPlaylistUI extends JFrame {
             }
         }
     }
+    // </editor-fold>
 
 }
